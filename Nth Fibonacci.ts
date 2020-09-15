fdde08334106319f1b5d3b7bd2952a61cc084be1
Hubert-Zhu <zhubolin010118@gmail.com>
2020-09-15 09:58:03 -0400

    Nth Fibonacci

---
+++ Nth Fibonacci.ts
@@ -0,0 +1,9 @@
+export function getNthFib(n: number):number {
+    if (n === 2){
+       return 1;
+   } else if (n === 1){
+       return 0;
+   } else {
+       return getNthFib(n - 1) + getNthFib(n -2)
+   }
+}
